1cb103460195ec0a604cb9e3bb761853170fd889
Yazan Alqadery <yazanalqadery@gmail.com>
2025-07-28 10:02:17 +0400

    Initial commit

---
+++ src/main/java/com/yzn/aml/detector/MoneyLaunderingDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.yzn.aml.detector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyLaunderingDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyLaunderingDetectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yzn/aml/detector/MoneyLaunderingDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzn.aml.detector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyLaunderingDetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
